2e99a2e5698e97a9128ed42716ff29d7c03728dc
Brandon <root@everdream.xyz>
2022-09-18 19:16:19 +0800

    add more job callbacks for telegram endpoints

--- diffusion_at_home/endpoints/telegram.py
+++ diffusion_at_home/endpoints/telegram.py
@@ -266,6 +266,35 @@ def build_resource_update_callback(app: BotServer):
     return tg_job_resource_update_callback
 
 
+def tg_build_failed_callback(app: BotServer):
+    async def callback(job: Job):
+        r = await app.telegram_session.post(
+            get_tg_endpoint('editMessageReplyMarkup'),
+            data={
+                'chat_id': job.chat_id,
+                'message_id': job.update_message_id,
+                'reply_markup': json.dumps({'inline_keyboard': []}),
+            },
+        )
+        logging.error(f'========= try removing inline keyboard, result: {r.json()}')
+
+    return callback
+
+
+def build_other_message_callback(app: BotServer):
+    async def callback(job: Job, message: str):
+        await app.telegram_sendMessage(
+            msg={
+                'chat_id': job.chat_id,
+                'text': message,
+                'reply_to_message_id': job.update_message_id,
+                'allow_sending_without_reply': True,
+            }
+        )
+
+    return callback
+
+
 async def tg_prompt_handler(app: BotServer, update: dict):
     chat_id = get_param(update, ['message', 'chat', 'id'])
     msg_id = get_param(update, ['message', 'message_id'])
@@ -309,6 +338,8 @@ async def tg_prompt_handler(app: BotServer, update: dict):
                 job_resource_update_callback=build_resource_update_callback(app),
                 job_progress_callback=tg_build_update_callback(app, chat_id, msg_id),
                 job_finish_callback=tg_build_finish_callback(app, chat_id, msg_id),
+                job_failed_callback=tg_build_failed_callback(app),
+                misc_message_callback=build_other_message_callback(app),
             )
             logging.info(f'Try adding new job {job.job_id} from chat {chat_id}')
             app.add_job(job)
